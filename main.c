4313985a203f0bca814ffe5cd756f7d9675dbe9c
steffen_b <steffen_b@8551bf61-fb70-47f8-9cdc-e5c233175072>
2009-11-09 07:29:55 +0000

    maintain compatibility if w/o pictures

--- main.c
+++ main.c
@@ -11,10 +11,16 @@ int main(int argc, char *argv[])
 	cProcessEpg *process ; 
 	process = new cProcessEpg();
 	process->confdir = string(argv[1]) ;
+#ifdef USE_IMAGEMAGICK
 	process->epgimagesdir = string(argv[2]) ;
+#endif
 	process->readMaps(process->confdir) ; 
-	
+
+#ifndef USE_IMAGEMAGICK	
+	for (n=2; n<argc; n++)
+#else
 	for (n=3; n<argc; n++)
+#endif
 	{
 		process->processFile(argv[n]);
 	}
