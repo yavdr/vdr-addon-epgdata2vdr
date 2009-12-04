92bb201afec9ee0911c7a05cab991d09ca9ff56d
megav0lt <megav0lt@8551bf61-fb70-47f8-9cdc-e5c233175072>
2009-12-04 15:46:04 +0000

    Fix Processing output\nCheck for  once at beginning

--- epgdata2vdr.sh
+++ epgdata2vdr.sh
@@ -10,13 +10,18 @@ UNZIPBIN=/usr/bin/unzip
 
 # install required files if necessary + create directories
 if [ ! -n "$WORKDIR" ]; then 
-   echo "Work directory undefined. Stopping."
-   exit 1 
+	echo "Work directory undefined. Stopping."
+	exit 1
 fi 
 
+if [ ! -n "$PIN" ]; then
+	echo "epgdata.com PIN is not configured. Stopping"
+	exit 1
+fi
+
 if [ ! -e $WORKDIR/include/epgdata2vdr_channelmap.conf ]; then
-   echo "epgdata2vdr_channelmap.conf not found. Stopping."
-   exit 1
+	echo "epgdata2vdr_channelmap.conf not found. Stopping."
+	exit 1
 fi
 
 if [ ! -d $WORKDIR/files ]; then
@@ -29,18 +34,12 @@ if [ ! -e $WORKDIR/include/genre.xml -o ! -e $WORKDIR/include/category.xml ]; th
       echo "$WORKDIR/include exists but is not a directory"
       exit 1
    else 
-    if [ -n "$PIN" ]; then 
       $CURLBIN "http://www.epgdata.com/index.php?action=sendInclude&iOEM=&pin=$PIN&dataType=xml" \
            -o $WORKDIR/include/include.zip
       $UNZIPBIN -o $WORKDIR/include/include.zip -d $WORKDIR/include
-    else
-      echo "epgdata.com PIN is not configured"
-      exit 1
-    fi
    fi
 fi
 
-if [ -n "$PIN" ]; then
 for i in `seq 0 $MAXDAYS` ; do
   TMP=`mktemp`
   $CURLBIN -I -D $TMP "http://www.epgdata.com/index.php?action=sendPackage&iOEM=VDR&pin=$PIN&dayOffset=$i&dataType=xml" &> /dev/null
@@ -64,9 +63,8 @@ for i in `seq 0 $MAXDAYS` ; do
       echo "File: $FILE already processed"
     else
       echo -e " File: $FILE  Size: $(( $SIZE /1024 )) kB"
-      # With EPG-Images
-      #epgdata2vdr          includedir        epgimagesdir file(s)
-      $EPGDATA2VDRBIN $WORKDIR/include/ $EPGIMAGES $WORKDIR/files/$FILE.zip 
+      #epgdata2vdr includedir epgimagesdir file(s)
+      $EPGDATA2VDRBIN $WORKDIR/include/ $EPGIMAGES $WORKDIR/files/$FILE.zip > $WORKDIR/files/$FILE.epg
       $SVDRPSENDBIN PUTE ${PUTECHAR}$WORKDIR/files/$FILE.epg
     fi
   fi
@@ -79,11 +77,6 @@ for i in `find $WORKDIR/files/* -name "*$SUFFIX.zip" | cut -d"_" -f2 | sort -r |
  rm -f $WORKDIR/files/*$i$SUFFIX.epg
  rm -f $WORKDIR/files/*$i$SUFFIX.zip
 done
-
-else
-  echo "epgdata.com PIN is not configured"
-  exit 1
-fi
 
 # Delete old EPG-Images
 if [ -e $WORKDIR/files/images/ ]; then 
